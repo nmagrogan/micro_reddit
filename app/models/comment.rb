5184065a4a9af4ce4b040b7515e10d32bec104c9
nmagrogan <nmagrogan@zagmail.gonzaga.edu>
2020-09-17 16:02:07 -0700

    updated comments to require foreign ids

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
   validates :content, presence: true
+  validates :post_id, presence: true
+  validates :user_id, presence: true
 
   belongs_to :user, optional: true
   belongs_to :comment, optional: true
